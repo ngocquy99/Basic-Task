b0d1050ea182e989dabdec0c761185b5bd243db2
Nguyen-Ngoc-Quy <ngocquy1619@gmail.com>
2023-03-17 20:39:09 +0700

    fist commit

---
+++ basic-ex/routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Http\Controllers\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['namespace' => 'App\Http\Controllers'], function()
+{   
+    /**
+     * Home Routes
+     */
+    Route::get('/', 'HomeController@index')->name('home.index');
+    // Route::post('/register-2', 'RegisterController@register')->name('register.perform');
+
+
+    Route::group([' ' => ['guest']], function() {
+        /**
+         * Register Routes
+         */
+        Route::get('/register', 'RegisterController@show')->name('register.show');
+
+        /**
+         * Login Routes
+         */
+        Route::get('/login', 'LoginController@show')->name('login.show');
+        Route::post('/login', 'LoginController@login')->name('login.perform');
+
+    });
+
+    Route::group(['middleware' => ['auth']], function() {
+        /**
+         * Logout Routes
+         */
+        Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+    });
+});
+
+
+Route::post('/register', [RegisterController::class, 'register'])->name('register.perform');
