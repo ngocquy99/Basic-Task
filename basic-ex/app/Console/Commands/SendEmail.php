71f84aba27c3eb2f4dcd446612218b6186f3595d
ngocqu1619 <ngocquy1619@gmail.com>
2023-03-24 16:54:22 +0700

    test demo cron add data to database table users

--- basic-ex/app/Console/Commands/SendEmail.php
+++ basic-ex/app/Console/Commands/SendEmail.php
@@ -3,23 +3,22 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\DB;
 
-class DemoCron extends Command
+class SendEamil extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'demo:cron';
+    protected $signature = 'send:email';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'add user';
+    protected $description = 'send email to users';
 
     /**
      * Create a new command instance.
@@ -38,12 +37,6 @@ class DemoCron extends Command
      */
     public function handle()
     {
-        DB::table('users')->insert([
-            'name' => 'bai viet 1',
-            'username' => 'Noi dung cua bai viet',
-            'email' => 'emailtestcron@gmail.com',
-            'password' => '11111111'
-        ]);
-        $this -> info('Demo:cron run successfuly');
+        return 0;
     }
 }
--- basic-ex/app/Console/Kernel.php
+++ basic-ex/app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use Commands\DemoCron;
 use App\Jobs\SendEmailJob;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
@@ -16,7 +17,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        'App\Console\Commands\DemoCron'
+        Commands\DemoCron::class,
     ];
     /**
      * Define the application's command schedule.
