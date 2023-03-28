d2b1d71e71105cd5b073b624b1cc2520271acd12
ngocqu1619 <ngocquy1619@gmail.com>
2023-03-28 11:31:50 +0700

    login and register center form

--- basic-ex/resources/views/auth/login.blade.php
+++ basic-ex/resources/views/auth/login.blade.php
@@ -4,21 +4,21 @@
     <form method="post" action="{{ route('login.perform') }}">
         
         <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-        <img class="mb-4" src="assets/Bootstrap_logo.svg.png" alt="logo" width="72" height="57">
+        <img class="mb-4" src="assets/Bootstrap_logo.svg.png" alt="logo" width="65" height="50">
         
         <h1 class="h3 mb-3 fw-normal">Login</h1>
 
 
-        <div class="form-group form-floating mb-3">
-            <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="Username" required="required" autofocus>
+        <div class=" container form-group form-floating mb-3 col-md-4 col-md-offset-5 align-center">
+            <input type="text" class="  form-control  " name="username" value="{{ old('username') }}" placeholder="Username" required="required" autofocus>
             <label for="floatingName">Email or Username</label>
             @if ($errors->has('username'))
                 <span class="text-danger text-left">{{ $errors->first('username') }}</span>
             @endif
         </div>
         
-        <div class="form-group form-floating mb-3">
-            <input type="password" class="form-control" name="password" value="{{ old('password') }}" placeholder="Password" required="required">
+        <div class=" container form-group form-floating mb-3 col-md-4 col-md-offset-5 align-center">
+            <input type="password" class=" form-control  " name="password" value="{{ old('password') }}" placeholder="Password" required="required">
             <label for="floatingPassword">Password</label>
             @if ($errors->has('password'))
                 <span class="text-danger text-left">{{ $errors->first('password') }}</span>
--- basic-ex/resources/views/noti.blade.php
+++ basic-ex/resources/views/noti.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="bg-light p-5 rounded">
     
-    <h1> Check mail for access</h1>
+    <h1> Check mail for access and verification</h1>
     
 
 </div>
